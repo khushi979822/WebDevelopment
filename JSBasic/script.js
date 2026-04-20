c1033c48a544e32ed042680d2d9402112ae02a71
khushi979822 <khushikumari5114@gmail.com>
2026-04-21 00:48:25 +0530

    Update background color for cravingInNumbers section and add initial HTML and JavaScript files

---
+++ JSBasic/script.js
@@ -0,0 +1,19 @@
+console.log("Hello World");
+
+
+let x = 10;
+let y = '10';
+
+console.log(x==y);
+console.log(x===y);
+console.log(x!=y);
+
+var k = 88;
+var l = '88';
+
+console.log(k==l);
+console.log(k===l);
+console.log(k!=l);
+
+console.log(typeof(k));
+console.log(typeof(l));
