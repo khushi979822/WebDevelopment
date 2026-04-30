3afeaff63f18a594337293c986e7f478cef253c5
khushi979822 <khushikumari5114@gmail.com>
2026-04-30 18:49:01 +0530

    Add HTML structure and JavaScript logic for conditional statements

---
+++ JSConditionalStatement/script.js
@@ -0,0 +1,21 @@
+let n = 5;
+if (n > 0) {
+  console.log("Positive Integer");
+} else {
+  console.log("Negative Integer");
+}
+
+let age = 20;
+if(age >= 18) {
+    console.log("You are eligible to vote.");
+} else {
+    console.log("You are not eligible to vote.");
+}
+
+let num = 10;
+if(num % 2 === 0) {
+    console.log("Even Number");
+} else {
+    console.log("Odd Number");
+}
+
