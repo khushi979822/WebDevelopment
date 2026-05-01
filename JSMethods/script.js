498b023c5365a38239ddc837323c1ebae48cf333
khushi979822 <khushikumari5114@gmail.com>
2026-05-01 18:48:41 +0530

    Add initial HTML structure and JavaScript functions for basic operations

---
+++ JSMethods/script.js
@@ -0,0 +1,12 @@
+let a = 20;
+b = 30;
+function sum(x, y) { // function with parameters
+  let z = x + y;
+  console.log(z);
+}
+sum(a, b);
+
+function welcome() { // function without parameters
+  console.log("Welcome to JavaScript Methods");
+}
+welcome();
