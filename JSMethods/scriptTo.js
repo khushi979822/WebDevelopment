c9479794f0dd4c0d18267fd9bed1ed4b0f1c1e4c
khushi979822 <khushikumari5114@gmail.com>
2026-05-01 19:41:14 +0530

    Add script references and arrow function implementations to scriptTo.js

--- JSMethods/scriptTo.js
+++ JSMethods/scriptTo.js
@@ -11,3 +11,15 @@ let add = function (x, y) {
 console.log(add); // function expression
 console.log(add(2, 3)); // function declaration
 
+const arrowfun = () => {
+  console.log("This is an arrow function");
+};
+arrowfun(); // calling the arrow function
+
+const arrowfun2 = (x, y) => {
+  return x + y;
+};
+console.log(arrowfun2(5, 10)); // calling the arrow function with parameters
+
+const arrowfun3 = (x, y) => x + y; // concise body syntax
+console.log(arrowfun3(7, 8)); // calling the concise arrow function
