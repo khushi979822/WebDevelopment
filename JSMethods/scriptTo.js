92e7dbe8cb11d3db030320b8d2d2ce2765b0434e
khushi979822 <khushikumari5114@gmail.com>
2026-05-01 19:16:21 +0530

    Add scriptTo.js with function expressions and basic operations; update index.html to reference new script

---
+++ JSMethods/scriptTo.js
@@ -0,0 +1,13 @@
+const abc = function () {
+  console.log("Hello Khushi");
+};
+
+console.log(abc); // function expression
+abc(); // calling the function
+
+let add = function (x, y) {
+  return x + y;
+};
+console.log(add); // function expression
+console.log(add(2, 3)); // function declaration
+
