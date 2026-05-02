1dde087cba1b6c779a5262b551513434ce6db23b
khushi979822 <khushikumari5114@gmail.com>
2026-05-02 19:39:39 +0530

    Refactor scope and hosting examples: replace script.js with new implementations and add script1.js; update index.html to reference script1.js

---
+++ JSScopeAndHoisting/script1.js
@@ -0,0 +1,23 @@
+let a;
+let b;
+let c;
+
+a = 10;
+console.log(a); // 10
+console.log(b); // undefined
+console.log(c); // undefined
+
+b = 15;
+console.log(a); // 10
+console.log(b); // 15
+console.log(c); // undefined
+
+c = 20;
+console.log(a); // 10
+console.log(b); // 15
+console.log(c); // 20
+
+abc(); // calling the function before its declaration (hoisting)
+function abc() {
+  console.log("Hey! Khushi");
+}
