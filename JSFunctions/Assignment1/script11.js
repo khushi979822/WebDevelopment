41a18d5a40ec6376a7e8988e219ed2b32506013b
khushi979822 <khushikumari5114@gmail.com>
2026-05-04 10:52:54 +0530

    Add index11.html and script11.js for JavaScript functions demonstration

---
+++ JSFunctions/Assignment1/script11.js
@@ -0,0 +1,6 @@
+function greet(name) {
+  console.log("Hello " + name + "!");
+}
+greet("Khushi"); // calling the function with an argument
+greet("Aman"); // calling the function with an argument
+greet("Parwati"); // calling the function with an argument
