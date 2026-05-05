1934fbe903daa4c6991b75ba0d8b4510370b30ff
khushi979822 <khushikumari5114@gmail.com>
2026-05-05 19:49:44 +0530

    Refactor array initialization and logging in script.js: uncomment relevant code and streamline array operations

--- Array_Using_JS/script.js
+++ Array_Using_JS/script.js
@@ -1,45 +1,43 @@
-//let arr = [10, 20, 30, 40, 50, 60, 70];
-// for (var i = 0; i < 7; i++) {
-//   console.log(arr[i]);
-// }
+let arr = [10, 20, 30, 40, 50, 60, 70];
+for (var i = 0; i < 7; i++) {
+  console.log(arr[i]);
+}
+//  console.log(arr);
+//  arr[2] = arr[2] + 10;
+//  console.log(arr);
+//  arr = [];
+//  console.log(arr);
+
+// let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90];
 // console.log(arr);
-// arr[2] = arr[2] + 10;
+
+// arr.push(99);
 // console.log(arr);
-// arr = [];
+
+// arr.pop();
 // console.log(arr);
 
-let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90];
-console.log(arr);
+// arr.unshift(88);
+// console.log(arr);
 
-arr.push(99);
-console.log(arr);
+// arr.shift(88);
+// console.log(arr);
 
-arr.pop();
-console.log(arr);
+// console.log(arr.at(4));
 
-arr.unshift(88);
-console.log(arr);
+// console.log(arr.includes(50));
 
-arr.shift(88);
-console.log(arr);
+// console.log(arr.indexOf(50));
 
-console.log(arr.at(4));
+// console.log(arr.findIndex((element) => element === 50));
 
-console.log(arr.includes(50));
+// let unar = [66,88,58,96,23,67,89,45,78];
+// console.log(unar);
+// unar.sort();
+// console.log(unar);
 
-console.log(arr.indexOf(50));
+// unar.reverse();
+// console.log(unar);
 
-console.log(arr.findIndex((element) => element === 50));
-
-let unar = [66,88,58,96,23,67,89,45,78];
-console.log(unar);
-unar.sort();
-console.log(unar);
-
-unar.reverse();
-console.log(unar);
-
-
-
-
-
+console.log("For Each Loop");
+arr.forEach((element) => console.log(element));
