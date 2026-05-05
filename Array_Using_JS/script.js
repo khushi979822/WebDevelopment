a6d84b047e466607401b36b685c465f5126e99dc
khushi979822 <khushikumari5114@gmail.com>
2026-05-05 19:22:46 +0530

    Refactor HTML and JavaScript: Update content in index.html and enhance array manipulation examples in script.js

--- Array_Using_JS/script.js
+++ Array_Using_JS/script.js
@@ -1,5 +1,25 @@
-let companies = ["Bloomberg", "Microsoft", "Uber", "Gogle", "IBM", "Netflix"];
-//it removes Bloomberg
-companies.shift();
-companies.splice(2, 0, "ola");
-companies.push("Amazon");
+//let arr = [10, 20, 30, 40, 50, 60, 70];
+// for (var i = 0; i < 7; i++) {
+//   console.log(arr[i]);
+// }
+// console.log(arr);
+// arr[2] = arr[2] + 10;
+// console.log(arr);
+// arr = [];
+// console.log(arr);
+
+let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+console.log(arr);
+
+arr.push(99);
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+arr.unshift(88);
+console.log(arr);
+
+arr.shift(88);
+console.log(arr);
+
