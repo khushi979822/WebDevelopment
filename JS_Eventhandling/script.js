a5eb51c1f2e49a2fbe99951ce1a5b1cdead89d6e
khushi979822 <khushikumari5114@gmail.com>
2026-05-07 20:03:10 +0530

    Add color input functionality and update bulb color dynamically

--- JS_Eventhandling/script.js
+++ JS_Eventhandling/script.js
@@ -17,3 +17,16 @@ function Success() {
 function Secondary() {
   document.getElementById("bulb1").style.backgroundColor = "blue";
 }
+
+document.getElementById("Warning").addEventListener("click", Warning());
+document.getElementById("Success").addEventListener("click", Success());
+document.getElementById("Secondary").addEventListener("click", Secondary());
+
+document
+  .getElementById("bulb1Color")
+  .addEventListener("change", ChangeBulbColor);
+
+function ChangeBulbColor() {
+  const color = document.getElementById("bulb1Color").value;
+  document.getElementById("bulb1").style.backgroundColor = color;
+}
