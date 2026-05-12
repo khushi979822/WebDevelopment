cf9dcbb5f52a727f135453ba76a50e5064126b9c
khushi979822 <khushikumari5114@gmail.com>
2026-05-12 20:24:40 +0530

    Enhance form validation with error messages and improved input structure

--- JSFormValidation/script.js
+++ JSFormValidation/script.js
@@ -1,16 +1,25 @@
 function submit() {
-  let fullName = document.getElementById("fullName").value;
-  let phone = document.getElementById("phone").value;
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
+  const fn = document.getElementById("fullName").value;
+  const em = document.getElementById("email").value;
+  const ph = document.getElementById("phone").value;
 
-  console.log("Name :", fullName);
-  console.log("Phone :", phone);
-  console.log("Email :", email);
-  console.log("Password :", password);
+  //   document.getElementById("nameError").innerText = "";
+  //   document.getElementById("emailError").innerText = "";
+  //   document.getElementById("phoneError").innerText = "";
 
-  document.getElementById("fullName").value = "";
-  document.getElementById("phone").value = "";
-  document.getElementById("email").value = "";
-  document.getElementById("password").value = "";
+  document.querySelectorAll(".error").forEach((element) => {
+    element.innerText = "";
+  });
+
+  if (!/^[A-Za-z\s]+$/.test(fn)) {
+    document.getElementById("nameError").innerText = "Invalid Full Name";
+  }
+
+  if (!/^[A-Za-z\.\_\d]+@gmail.com$/.test(em)) {
+    document.getElementById("emailError").innerText = "Invalid Email";
+  }
+
+  if (!/^[6-9]\d{9}$/.test(ph)) {
+    document.getElementById("phoneError").innerText = "Invalid Phone";
+  }
 }
