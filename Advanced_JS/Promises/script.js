10745d9b14451f4e34ede5e424c49cce7e911c9d
khushi979822 <khushikumari5114@gmail.com>
2026-05-19 19:35:32 +0530

    Implement getData function with Promise for asynchronous data retrieval

---
+++ Advanced_JS/Promises/script.js
@@ -0,0 +1,19 @@
+function getData(value) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (value === 3) {
+        reject("Query not Found");
+      } else {
+        console.log("Data Sent", value);
+        resolve("Query Solved");
+      }
+    }, 2000);
+  });
+}
+
+getData(1)
+  .then(() => getData(2))
+  .then(() => getData(3))
+  .then(() => getData(4))
+  .then(() => getData(5))
+  .catch((rej) => console.log(rej));
