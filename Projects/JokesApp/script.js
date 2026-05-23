fc137bdb488f0249fc6f91a6151427d0051f0bcc
khushi979822 <khushikumari5114@gmail.com>
2026-05-23 21:00:00 +0530

    Add new elements to display joke type and ID; update API URL in GetNewJoke function

--- Projects/JokesApp/script.js
+++ Projects/JokesApp/script.js
@@ -1,8 +1,9 @@
 async function GetNewJoke() {
-  const API_URL = "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark";
+  const API_URL = "https://official-joke-api.appspot.com/jokes/random";
 
   const response = await fetch(API_URL);
-  // console.log(response);
+   console.log(response);
+
 
   const data = await response.json();
   // console.log(data);
@@ -15,6 +16,8 @@ async function GetNewJoke() {
   //     document.getElementById("delivery").innerText = data.delivery;
   //   }
 
-  document.getElementById("setup").innerText = data.joke || data.setup;
-  document.getElementById("delivery").innerText = data.delivery || "";
+  document.getElementById("setup").innerText = "Setup : " +  (data.joke || data.setup);
+  document.getElementById("delivery").innerText = "Punchline : " +  (data.punchline || "");
+  document.getElementById("type").innerText = "Type: " + data.type;
+  document.getElementById("id").innerText = "ID: " + data.id;
 }
