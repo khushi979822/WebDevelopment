c04f45a74dfc3842bb3f4cc4d0bea7602a95c7c3
khushi979822 <khushikumari5114@gmail.com>
2026-06-01 19:15:37 +0530

    Refactor App component layout, enhance Header with navigation, and add new page components for Home, About, Contact Us, Login, Register, and Product

--- Frontend/myFirstReactApp/src/App.jsx
+++ Frontend/myFirstReactApp/src/App.jsx
@@ -6,19 +6,7 @@ function App() {
   return (
     <>
       <Header />
-      <br />
-      <div id="abcd">My First react app</div>
-      <br />
-      <p className="bg-success">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat
-        pariatur adipisci, hic maxime sit doloribus ipsa. Reprehenderit
-        provident nemo, doloribus, nostrum corrupti quam, eveniet unde officiis
-        est error debitis nam quo? Veniam, a. At officia quasi exercitationem
-        enim eum aliquam maiores autem expedita aspernatur quisquam!
-      </p>
-      <button id="clickButton">Click Here</button>
-      <br />
-      <br />
+    
       <Footer />
     </>
   );
--- Frontend/myFirstReactApp/src/components/Header.jsx
+++ Frontend/myFirstReactApp/src/components/Header.jsx
@@ -1,7 +1,22 @@
 function Header() {
   return (
     <>
-      <div id="header">This is my Header</div>
+      <div className="bg-primary-subtle p-2 d-flex justify-content-between">
+        <div className="text-primary fs-4 fw-bold">My Company</div>
+          <div className="d-flex gap-4">
+            <span>Home</span>
+            <span>About</span>
+            <span>Products</span>
+            <span>Contact us</span>
+          </div>
+          <div className="d-flex gap-3">
+            <button className="btn btn-outline-primary">Login</button>
+            <button className="btn btn-primary">Register</button>
+          </div>
+
+
+
+      </div>
     </>
   );
 }
