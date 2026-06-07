cbb1b9243589cb63a0308f1f157f04741ac1f091
khushi979822 <khushikumari5114@gmail.com>
2026-06-07 15:08:01 +0530

    feat: update Home page content and styling for improved user engagement

--- Frontend/myFirstReactApp/src/pages/Home.jsx
+++ Frontend/myFirstReactApp/src/pages/Home.jsx
@@ -7,11 +7,13 @@ function Home() {
     <>
       <div className="container d-flex justify-content-center align-items-center">
         <div className="border p-3 w-50 mt-5 bg-light shadow rounded">
-          <h1 className="text-center">Welcome to Home Page!!!</h1>
+          <h1 className="text-center text-primary">Welcome to Home Page!!!</h1>
 
-          <p className="text-center">
-            We have wide range of Clothings, Footwear and Accessories for you.
-            Please check our products and enjoy shopping with us.
+          <p className="text-center mt-4">
+            This is the home page of our application. You can explore our
+            products and services by clicking the button below. We offer a wide
+            range of products that cater to your needs. Feel free to browse
+            through our collection and find exactly what you're looking for.
           </p>
 
           <div className="d-flex justify-content-center mt-4">
