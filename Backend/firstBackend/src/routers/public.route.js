fb1f1e7df5e3b7bbe2f2888c4cb57b102842b7f0
khushi979822 <khushikumari5114@gmail.com>
2026-06-22 20:24:00 +0530

    feat: implement authentication and public contact routes with corresponding controllers

--- Backend/firstBackend/src/routers/public.route.js
+++ Backend/firstBackend/src/routers/public.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import { ContactUs } from '../controllers/public.controller.js';
 
 const router = express.Router();
 
 
-router.post("/contact-us");
+router.post("/contact-us",ContactUs);
 
 export default router;
