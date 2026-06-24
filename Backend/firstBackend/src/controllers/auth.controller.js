a4a57ad681f5f8eb61797af0c07409936d6242de
khushi979822 <khushikumari5114@gmail.com>
2026-06-24 23:19:54 +0530

    feat: add auth controller and user model
    - Created user model for authentication
    - Updated database connection configuration
    - Added authentication controller logic

--- Backend/firstBackend/src/controllers/auth.controller.js
+++ Backend/firstBackend/src/controllers/auth.controller.js
@@ -1,12 +1,28 @@
- export const LoginUser = (req, res) => {
+import User from "../models/user.model.js";
+
+export const RegisterUser = async (req, res) => {
+  try {
+    const { fullName, email, phone, gender, password, dob } = req.body;
+
+    if (!fullName || !email || !phone || !gender || !password || !dob) {
+      res.status(400).json({ message: "All fields are required" });
+      return;
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      res.status(409).json({ message: "Email Already Registered" });
+    }
+
+    // Create New User and Complete registration will do this tomorrow
+  } catch (error) {}
+};
+
+export const LoginUser = (req, res) => {
   res.json({ message: "Login Successfull from Controller" });
 };
 
 export const LogoutUser = (req, res) => {
   res.json({ message: "Logout Successfull from Controller" });
 };
-
-export const RegisterUser = (req, res) => {
-  res.json({ message: "Register Successfull from Controller" });
-};
-
