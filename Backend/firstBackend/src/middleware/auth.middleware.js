efa24939eee050707212aadfaedb1d012935613d
khushi979822 <khushikumari5114@gmail.com>
2026-06-27 13:49:22 +0530

    feat: add middleware for authentication and enhance route handling

---
+++ Backend/firstBackend/src/middleware/auth.middleware.js
@@ -0,0 +1,7 @@
+export const sampleMiddleWare = async (req, res, next) => {
+  console.log("I am Sample Middleware 1");
+};
+
+export const sampleMiddleWare2 = async (req, res, next) => {
+  console.log("I am Sample Middleware 2");
+};
